db1a2af05747ce3d5489b71dde60a9c21aee849e
nkaaf <nkaaf@protonmail.com>
2021-07-06 23:09:25 +0200

    fix config update

--- nginx/scripts/update_https_config.sh
+++ nginx/scripts/update_https_config.sh
@@ -8,11 +8,11 @@ _init_script "$0"
 . "$_scripts_dir/common/i18n_core.sh"
 _init_i18n
 
-if ! [ -f "/usr/local/share/exchange/actions/get.action" ]; then
-  _error "$(_translate i18n_ERROR_FILE_NOT_EXISTS) /usr/local/share/exchange/actions/get.action"
+if ! [ -f "/usr/local/share/exchange/actions/update.action" ]; then
+  _error "$(_translate i18n_ERROR_FILE_NOT_EXISTS) /usr/local/share/exchange/actions/update.action"
 fi
 
-_action=$(cat "/usr/local/share/exchange/actions/get.action")
+_action=$(cat "/usr/local/share/exchange/actions/update.action")
 _certificate=$(echo "$_action" | cut -d "/" -f1)
 _domains_count=$(echo "$_action" | grep -o "/" | wc -l)
 _domains=$(echo "$_action" | cut -d "/" -f2-"$((_domains_count + 1))" | sed "s|/| |g")
